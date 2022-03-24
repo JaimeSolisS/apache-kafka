7a476595cca24d04ebc35f5be2950c1e033c37bf
Jaime Solis <jaimesolissoto@me.com>
2022-03-24 14:21:22 -0700

    feat streams application

--- 04-Kafka_Streams/06-Exactly_Once_Semantics/BankBalance-ExactlyOnce/src/main/java/org/jsolis/kafka/streams/BankBalanceStreamsApp.java
+++ 04-Kafka_Streams/06-Exactly_Once_Semantics/BankBalance-ExactlyOnce/src/main/java/org/jsolis/kafka/streams/BankBalanceStreamsApp.java
@@ -1,7 +1,92 @@
 package org.jsolis.kafka.streams;
 
+
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.connect.json.JsonSerializer;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+
+import java.time.Instant;
+import java.util.Properties;
+
 public class BankBalanceStreamsApp {
     public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "bankbalance-application");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        /*
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        */
 
+        // we disable the cache to demonstrate all the "steps" involved in the transformation not recommended in prod
+        properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0");
+
+        // exactly once processing
+        properties.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
+
+        // json Serde
+        final Serializer<JsonNode> jsonSerializer = new JsonSerializer();
+        final Deserializer<JsonNode> jsonDeserializer = new JsonDeserializer();
+        final Serde<JsonNode> jsonSerde = Serdes.serdeFrom(jsonSerializer, jsonDeserializer);
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, JsonNode> bankTransactions = builder.stream("bank-transactions", Consumed.with(Serdes.String(), jsonSerde));
+
+        // create the initial json object for balances
+        ObjectNode intialBalance = JsonNodeFactory.instance.objectNode();
+        intialBalance.put("count", 0);
+        intialBalance.put("balance", 0);
+        intialBalance.put("time", Instant.ofEpochMilli(0L).toString());
+
+        KTable<String, JsonNode> bankBalance = bankTransactions
+                .groupByKey(Serialized.with(Serdes.String(), jsonSerde))
+                .aggregate(
+                        () -> intialBalance,
+                        (key, transaction, balance) -> newBalance(transaction, balance),
+                        Materialized.<String, JsonNode, KeyValueStore<Bytes, byte[]>>as("bank-balance-agg")
+                                .withKeySerde(Serdes.String())
+                                .withValueSerde(jsonSerde)
+                );
+
+        bankBalance.toStream().to("bank-balance-exactly-once", Produced.with(Serdes.String(), jsonSerde));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), properties);
+        streams.start();
+
+        //print topology
+        System.out.println(streams.toString());
+
+        // shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook (new Thread(streams::close));
+    }
+
+    private static JsonNode newBalance(JsonNode transaction, JsonNode oldBalance){
+        // create new JSON object
+        ObjectNode newBalance = JsonNodeFactory.instance.objectNode();
+        newBalance.put( "count", oldBalance.get("count").asInt () + 1);
+        newBalance.put(  "balance", oldBalance.get("balance").asInt() + transaction.get("amount").asInt());
+
+        Long balanceEpoch = Instant.parse(oldBalance.get("time").asText()).toEpochMilli();
+        Long transactionEpoch = Instant.parse(transaction.get("time").asText ()).toEpochMilli();
+        Instant newBalanceInstant = Instant.ofEpochMilli(Math.max (balanceEpoch, transactionEpoch));
+        newBalance.put("time", newBalanceInstant.toString());
+        return newBalance;
     }
 }
