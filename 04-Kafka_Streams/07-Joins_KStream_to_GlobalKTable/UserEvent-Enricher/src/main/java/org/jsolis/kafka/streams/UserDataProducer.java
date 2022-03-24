bc605b13ea5909e2d72e00f687790b06a4c5d553
Jaime Solis <jaimesolissoto@me.com>
2022-03-24 16:49:44 -0700

    setup project

---
+++ 04-Kafka_Streams/07-Joins_KStream_to_GlobalKTable/UserEvent-Enricher/src/main/java/org/jsolis/kafka/streams/UserDataProducer.java
@@ -0,0 +1,7 @@
+package org.jsolis.kafka.streams;
+
+public class UserDataProducer {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 04-Kafka_Streams/07-Joins_KStream_to_GlobalKTable/UserEvent-Enricher/src/main/java/org/jsolis/kafka/streams/UserEventEnricherApp.java
@@ -0,0 +1,7 @@
+package org.jsolis.kafka.streams;
+
+public class UserEventEnricherApp {
+    public static void main(String[] args) {
+
+    }
+}
